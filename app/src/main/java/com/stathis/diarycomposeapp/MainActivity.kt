1e559f4f128b1046b4c9c995e5ecca1d54d33261
Stathis Karadimitriou <stathis.karadimitriou@gmail.com>
2024-05-18 13:28:32 +0300

    Implementation: setup navigation logic

--- app/src/main/java/com/stathis/diarycomposeapp/MainActivity.kt
+++ app/src/main/java/com/stathis/diarycomposeapp/MainActivity.kt
@@ -3,27 +3,24 @@ package com.stathis.diarycomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.rememberNavController
+import com.stathis.diarycomposeapp.navigation.Screen
+import com.stathis.diarycomposeapp.navigation.SetupNavGraph
 import com.stathis.diarycomposeapp.ui.theme.DiaryComposeAppTheme
 
 class MainActivity : ComponentActivity() {
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         setContent {
             DiaryComposeAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    //
-                }
+                val navController = rememberNavController()
+                SetupNavGraph(
+                    startDestination = Screen.Authentication.route,
+                    navController = navController
+                )
             }
         }
     }
---
+++ app/src/main/java/com/stathis/diarycomposeapp/util/Consts.kt
@@ -0,0 +1,3 @@
+package com.stathis.diarycomposeapp.util
+
+const val WRITE_SCREEN_ARG_KEY = "diaryId"
