ce2f5c6adfea13aa94a647b20f58872f1e5f7046
Stathis Karadimitriou <stathis.karadimitriou@gmail.com>
2024-05-18 13:09:26 +0300

    Implementation: splash screen using splash screen api

--- app/src/main/java/com/stathis/diarycomposeapp/MainActivity.kt
+++ app/src/main/java/com/stathis/diarycomposeapp/MainActivity.kt
@@ -7,12 +7,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.stathis.diarycomposeapp.ui.theme.DiaryComposeAppTheme
 
 class MainActivity : ComponentActivity() {
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             DiaryComposeAppTheme {
                 // A surface container using the 'background' color from the theme
